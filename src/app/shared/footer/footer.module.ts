20e0bd5f94362c0714dfab41cbc7d9018b4d2f02
phamchung <phamchung@MacBook-Pro-cua-admin.local>
2022-03-21 14:10:36 +0700

    fix

--- src/app/shared/footer/footer.module.ts
+++ src/app/shared/footer/footer.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer/footer.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 
 
@@ -9,7 +10,8 @@ import { FooterComponent } from './footer/footer.component';
     FooterComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FontAwesomeModule,
   ],
   exports: [
     FooterComponent
